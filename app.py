e21f4302f401c3d4f0999c4e81f895ca67985029
Ratik Puri <ratikpuri1998@gmail.com>
2020-05-12 01:14:14 +0530

    Updated port and requirements

--- app.py
+++ app.py
@@ -106,6 +106,6 @@ def internal_server_error(error):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
 
     app.run(debug=False,port=port)
