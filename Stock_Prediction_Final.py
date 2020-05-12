50a0065bcdff8f2f13f5124c07e1fcdfca222261
Ratik Puri <ratikpuri1998@gmail.com>
2020-05-12 23:07:10 +0530

    minor change

--- Stock_Prediction_Final.py
+++ Stock_Prediction_Final.py
@@ -454,7 +454,7 @@ def retrieving_tweets_polarity(symbol):
 
 def polarity(symbol):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth.set_access_token(access_token, access_secret)
     user = tweepy.API(auth)
 
     tweets = tweepy.Cursor(user.search, q=str(symbol), tweet_mode='extended', lang='en').items(num_of_tweets)
